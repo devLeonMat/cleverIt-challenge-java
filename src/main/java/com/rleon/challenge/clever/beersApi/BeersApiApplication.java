5e3bc9f557d79d82878c32544c71829b617f67c2
Leon Matias <roslin.pineda@nautilus.team>
2021-12-14 23:17:36 -0500

    add project

---
+++ src/main/java/com/rleon/challenge/clever/beersApi/BeersApiApplication.java
@@ -0,0 +1,13 @@
+package com.rleon.challenge.clever.beersApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rleon/challenge/clever/beersApi/BeersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.challenge.clever.beersApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
